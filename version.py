5b4cdce81501da18a1ab6a487bffae2e5cd71e24
Sparki1337 <cernicenkodana@gmail.com>
2025-09-30 08:20:17 +0300

    release: v1.3.0 stable

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 """Информация о версии приложения."""
 
 __version__ = "1.3.0"
-__status__ = "Beta"  # Beta, Stable, RC
+__status__ = "Stable"  # Beta, Stable, RC
 
 def get_version_string() -> str:
     """Получить полную строку версии."""
