f8922581acd5fd6e24ec966a308335a93e0b2674
Sparki1337 <cernicenkodana@gmail.com>
2025-10-01 14:02:44 +0300

    chore: set status to Stable

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 """Информация о версии приложения."""
 
-__version__ = "1.3.0"
-__status__ = "Beta"  # Beta, Stable, RC
+__version__ = "1.4.0"
+__status__ = "Stable"  # Beta, Stable, RC
 
 def get_version_string() -> str:
     """Получить полную строку версии."""
