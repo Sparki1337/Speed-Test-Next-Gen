b86e8548d1ff54badd51db667b2492ffcea19738
Sparki1337 <cernicenkodana@gmail.com>
2025-10-08 08:11:55 +0300

    chore: update comments

--- version.py
+++ version.py
@@ -1,13 +1,13 @@
 # coding: utf-8
-"""Информация о версии приложения."""
+# Информация о версии приложения.
 
 __version__ = "1.3.2"
 __status__ = "Beta"  # Beta, Stable, RC
 
 def get_version_string() -> str:
-    """Получить полную строку версии."""
+    # Получить полную строку версии.
     return f"v{__version__} | {__status__}"
 
 def get_window_title() -> str:
-    """Получить заголовок окна."""
+    # Получить заголовок окна.
     return f"Speedtest NextGen by Sparki | {get_version_string()}"
